d515e8c1f7f1747db03495ba2a99fc3424fa2c97
Tran Minh Hieu <hieussob@gmail.com>
2025-04-02 18:47:24 +0700

    update homecontroller

--- Logistic_Website/Logistic_Website/Controllers/HomeController.cs
+++ Logistic_Website/Logistic_Website/Controllers/HomeController.cs
@@ -1,21 +1,59 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Logistic_Website.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using X.PagedList.Extensions;
 
 namespace Logistic_Website.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private List<VesselSchedule> _vesselSchedules;
+    private List<VesselSchedule> _filteredSchedules;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        _vesselSchedules = TransientData.GetScheduleDetails();
     }
 
     public IActionResult Index()
     {
+        var ports = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "HCM", Text = "Hồ Chí Minh" },
+            new SelectListItem { Value = "HN", Text = "Hà Nội" },
+            new SelectListItem { Value = "DN", Text = "Đà Nẵng" },
+            new SelectListItem { Value = "HAIPHONG", Text = "Hải Phòng" },
+            new SelectListItem { Value = "SINGAPORE", Text = "Singapore" }
+        };
+
+        ViewBag.Ports = ports;
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult Search(string origin, string destination, int? page)
+    {
+        TransientData.FilterVesselChedules = _vesselSchedules.Where(s => s.Origin == origin && s.Destination == destination).ToList();
+
+        int pageSize = 3;
+        int pageNumber = page ?? 1; 
+        var pageFilterSchedule = TransientData.FilterVesselChedules.ToPagedList(pageNumber, pageSize);
+
+        return View(pageFilterSchedule);
+    }
+
+    [HttpGet]
+    public IActionResult Search(int? page)
+    {
+        int pageSize = 3;
+        int pageNumber = page ?? 1;
+        var pageFilterSchedule = TransientData.FilterVesselChedules.ToPagedList(pageNumber, pageSize);
+
+        return View(pageFilterSchedule);
     }
 
     public IActionResult Privacy()
---
+++ Logistic_Website/Logistic_Website/Models/VesselSchedule.cs
@@ -0,0 +1,20 @@
+﻿namespace Logistic_Website.Models
+{
+    public class VesselSchedule
+    {
+        public int Id { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string Line { get; set; }
+        public string Service { get; set; }
+        public string Vessel { get; set; }
+        public string CutOff { get; set; }
+        public string ETDHPH { get; set; }
+        public string POD { get; set; }
+        public string MV { get; set; }
+        public string ETD { get; set; }
+        public string ETA { get; set; }
+        public string TransitTime { get; set; }
+        public string ETSIN { get; set; }
+    }
+}
